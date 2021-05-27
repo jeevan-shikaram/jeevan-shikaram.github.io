8532aaf60d719070b3379bd875b1ce32c45b9cf1
Jeevan Shikaram <jeevan-shikaram@users.noreply.github.com>
2021-05-27 16:25:09 +1000

    Add Share Target support.

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,13 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function (event) {
+    const url = new URL(event.request.url);
+    if (url.pathname === '/share') {
+        const responsePromise = event.request.formData().then((formData) => {
+            console.log('Title: ' + formData.get('title'));
+            console.log('Text: ' + formData.get('text'));
+            console.log('File: ' + formData.get('file'));
+
+            return Response.redirect('/');
+        });
+        event.respondWith(responsePromise);
+    }
+});
